64458adf05d345be63ea6414cd8caee58321fe2e
Justin Johnson <j26774@gmail.com>
2016-01-29 09:22:53 -0800

    fixed typo

--- neural_style.lua
+++ neural_style.lua
@@ -232,7 +232,7 @@ local function main(params)
   for i=1,#net.modules do
     local module = net.modules[i]
     if torch.type(module) == 'nn.SpatialConvolutionMM' then
-        -- remote these, not used, but uses gpu memory
+        -- remove these, not used, but uses gpu memory
         module.gradWeight = nil
         module.gradBias = nil
     end
