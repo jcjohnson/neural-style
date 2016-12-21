fa058423c4e788553cdb1b9ab9f244a5c3b6b220
Mikhail Merkulov <mikhailm@benders-Mac-mini-7.local>
2016-12-21 16:07:32 +0200

    fixes

--- run_neural_style.py
+++ run_neural_style.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from __future__ import print_function
 import os, sys, subprocess
 from datetime import datetime
 from argparse import ArgumentParser
@@ -204,8 +203,8 @@ def printProgress(iteration, total, prefix='', suffix='', decimals=1, barLength=
     formatStr = "{0:." + str(decimals) + "f}"
     percent = formatStr.format(100 * (iteration / float(total)))
     filledLength = int(round(barLength * iteration / float(total)))
-    bar_str = '█' * filledLength + '-' * (barLength - filledLength)
-    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar_str, percent, '%', suffix)),
+    bar = '█' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
     if iteration == total:
         sys.stdout.write('\n')
     sys.stdout.flush()
